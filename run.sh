ac512a4a0c3bd414106e158adbd1c07a1f3f4fc9
Cheng Li <chenglii@cs.rutgers.edu>
2013-10-28 16:28:42 -0400

    Update bash file;

--- run.sh
+++ run.sh
@@ -7,8 +7,26 @@ function run {
 			PID=$!
 		done
 	done
-	
 	wait $PID
 }
 
-time run
+function measure {
+	TRACE=$1
+	OUTDIR=$2
+	for sjf in `echo 0.0 0.25 0.5 0.75 1.0`; do
+		#for approx in 1.0; do
+		for drop in 0 25 50 75 100; do
+			OUTFILE=$OUTDIR/res_D$drop\_S$sjf
+			#pypy runsimulator.py  --approx $approx --sjf $sjf -l $OUTFILE 
+			pypy runsimulator.py -f $TRACE -d $drop -s $sjf -l $OUTFILE 
+			perl parse_hadoop_sim.pl $OUTFILE > $OUTFILE\.dat
+			python ../../repos/create-cdf-data.py $OUTFILE\.dat > $OUTFILE\.res
+			PID=$!
+		done
+	done
+	wait $PID
+}
+
+##time ru
+time measure $*
+
