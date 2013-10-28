c0f1666e6c810837674efc2559cac658c5ea5438
Inigo Goiri <elgoiri@gmail.com>
2013-10-28 13:34:51 -0400

    Added structure for scheduling policies

--- simulator.py
+++ simulator.py
@@ -67,6 +67,29 @@ class Simulator:
 		
 		return job.jobId
 	
+	'''
+	Scheduling policies.
+	Returns the order of 2 jobs according to the scheduling policy
+	'''
+	def schedulingPolicy(self, jobId1, jobId2):
+		return self.schedulingFIFOprior(jobId1, jobId2)
+		
+		
+	def schedulingFIFO(self, jobId1, jobId2):
+		return self.jobs[jobId1].submit-self.jobs[jobId2].submit
+	
+	def schedulingFIFOprior(self, jobId1, jobId2):
+		if self.jobs[jobId1].priority==self.jobs[jobId2].priority:
+			return self.jobs[jobId1].submit-self.jobs[jobId2].submit
+		else:
+			return self.jobs[jobId2].priority-self.jobs[jobId1].priority
+	
+	def schedulingSJF(self, jobId1, jobId2):
+		# TODO cheng
+		# One approach can be to check length of the jobs in the queue
+		# Sort it according to the length
+		return self.jobs[jobId2].nmaps*self.jobs[jobId2].lmap - self.jobs[jobId1].nmaps*self.jobs[jobId1].lmap
+	
 	# Check if there is any idle node for reduces
 	def getIdleNodeMap(self):
 		for nodeId in sorted(self.nodes):
